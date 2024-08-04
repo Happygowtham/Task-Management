4a2398c19e161a63622afff9c66f3153c10dc41f
Happygowtham <gowtham06787@gmail.com>
2024-08-04 16:19:34 +0530

    email updated

--- backend/config.js
+++ backend/config.js
@@ -2,6 +2,6 @@ module.exports = {
     JWT_SECRET: 'TASK-MANAGEMENT-SECRET-KEY',
     EMAIL_HOST: 'smtp.gmail.com',
     EMAIL_PORT: 587,
-    EMAIL_USER: 'test@email.com', //Replace with the Original Email
-    EMAIL_PASS: 'test' //Replace with the Original Password
+    EMAIL_USER: 'gowtham@dbcyelagiri.edu.in',
+    EMAIL_PASS: 'bsoft@2051'
 };
